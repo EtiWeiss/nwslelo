574b2dd958776b8c7e07acadbc1eed7b8e33bb00
Eti Weiss <etiweiss@duck.com>
2024-05-26 02:13:59 -0400

    finished python code

--- elo.py
+++ elo.py
@@ -6,15 +6,75 @@ def get_elo(team):
         host="127.0.0.1",
         user="root",
         password="iTTia1!Rd",
-        database="teams"
+        database="nwslelo"
     )
     cursor = conn.cursor()
     query = "SELECT elo FROM nwslelo.teams WHERE team = %s"
     cursor.execute(query, (team,))
-    elo = cursor.fetchone()[3]  # Assuming score is in the first column
+    elo = cursor.fetchone()[0]
     cursor.close()
     conn.close()
     return elo
+
+def update_elo(elo, team):
+    conn = mysql.connector.connect(
+        host="127.0.0.1",
+        user="root",
+        password="iTTia1!Rd",
+        database="nwslelo"
+    )
+    cursor = conn.cursor()
+    query = "UPDATE nwslelo.teams SET elo = %s WHERE team = %s"
+    cursor.execute(query, (elo, team))
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+def update_matchups(Team1,ELO1,Team2,Elo2,Result1,Result2,Elo1new,Elo2new):
+    conn = mysql.connector.connect(
+        host="127.0.0.1",
+        user="root",
+        password="iTTia1!Rd",
+        database="nwslelo"
+    )
+    cursor = conn.cursor()
+    query = "INSERT INTO nwslelo.matchups VALUE(%s,%s,%s,%s,%s,%s,%s,%s)"
+    cursor.execute(query, (Team1,ELO1,Team2,Elo2,Result1,Result2,Elo1new,Elo2new))
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+team1 = input('Who was the first team? ')
+elo_not1 = get_elo(team1)
+team2 = input('Who was the second team? ')
+elo_not2 = get_elo(team2)
+print(f"The current elo for {team1} is {elo_not1} and {team2} is {elo_not2}")
+
 #get score
+result1 = float(input('What was the outcome? Enter 0 for the first team losing, 1 for first team winning, or 0.5 for a draw: '))
+result2 = 1 - result1
+if result1 > result2:
+    print(f'{team1} beat {team2}')
+elif result2 > result1:
+    print(f'{team2} beat {team1}')
+else:
+    print(f'{team1} and {team2} drew')
+
 #run calc
-#update elo
+Qa = 10**(elo_not1/400)
+Qb = 10**(elo_not2/400)
+Ea = Qa/(Qa+Qb)
+Eb = Qb/(Qa+Qb)
+elo1new = int(elo_not1 + (32*(result1 - Ea)))
+elo2new = int(elo_not2 + (32*(result2 - Eb)))
+print(f"The new elo for {team1} is {elo1new} and {team2} is {elo2new}")
+commit = input('Is this correct? Y/N: ')
+
+if commit == 'Y':
+    update_elo(elo1new,team1) #update elo
+    update_elo(elo2new,team2) #update elo
+    update_matchups(team1,elo_not1,team2,elo_not2,result1,result2,elo1new,elo2new) #update matchup
+    print('Elo has been updated!')
+else:
+    print('Start program again and enter correct info. If problem persists, contact Support')
+
---
+++ test.py
@@ -0,0 +1,9 @@
+import json
+#x = '{"team1" : "5","team2" : "10","points1" : 0,"points2" : 1}'
+#y = json.loads(x)
+#print(y["points2"])
+
+#with open('score.json') as json_file:
+#    data = json.load(json_file)
+
+#print(data)
